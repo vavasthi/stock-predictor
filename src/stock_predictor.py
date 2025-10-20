Fixed issues with scaling

src/stock_predictor.py
@@ -18,17 +18,17 @@ class StockPredictor(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         outputs, loss = self.common_step(batch, batch_idx)
-        self.log('train_loss', loss)
+        self.log('train_loss', loss, prog_bar = True, logger = True, on_epoch=True, on_step=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         outputs, loss = self.common_step(batch, batch_idx)
-        self.log('val_loss', loss)
+        self.log('val_loss', loss, prog_bar = True, logger = True, on_epoch=True, on_step=True)
         return loss
 
     def test_step(self, batch, batch_idx):
         outputs, loss = self.common_step(batch, batch_idx)
-        self.log('test_loss', loss)
+        self.log('test_loss', loss, prog_bar = True, logger = True, on_epoch=True, on_step=True)
         return loss
 
     def common_step(self, batch, batch_idx):
@@ -37,6 +37,7 @@ class StockPredictor(pl.LightningModule):
         loss = 0
         if targets is not None:
             loss = self.criterion(outputs, targets)
+        print('Loss is ', loss)
         predictions = torch.argmax(outputs, dim=1)
         return outputs, loss
 

src/train.py
@@ -1,4 +1,7 @@
-from pytorch_lightning.callbacks import ModelCheckpoint
+from argparse import ArgumentParser
+
+from pytorch_lightning.callbacks import ModelCheckpoint,RichProgressBar
+
 from pytorch_lightning.loggers import TensorBoardLogger
 import pytorch_lightning as pl
 
@@ -10,7 +13,7 @@ from utilities import populate_scaler
 import os
 
 
-def run(base_directory):
+def run(base_directory, accelerator, devices):
     has_mps = torch.backends.mps.is_built()
     device = "mps" if has_mps else "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
@@ -29,9 +32,13 @@ def run(base_directory):
     logger = TensorBoardLogger('lightning_logs',
                                name="stock_predictor")
 
-    trainer = pl.Trainer(logger=logger, callbacks=[checkpoint_callback], max_epochs=1)
+    trainer = pl.Trainer(logger=logger, accelerator=accelerator,
+                         callbacks=[checkpoint_callback, RichProgressBar()], max_epochs=1)
+    if devices != None:
+        trainer = pl.Trainer(logger=logger, accelerator=accelerator, devices=devices, callbacks=[checkpoint_callback, RichProgressBar()], max_epochs=1)
 
     model = StockPredictor(device).to(device)
+    model = torch.compile(model)
     data_module = StockPredictorDataModule(base_directory=base_directory,
                                            device=device,
                                            train_workers=15,
@@ -49,4 +56,8 @@ if __name__ ==  '__main__':
     mp.set_start_method('spawn', force=True)
 #    base_directory = "/data/datasets/stockPredictor"
     base_directory = "/data/datasets/stockPredictor"
-    run(base_directory)
+    parser = ArgumentParser()
+    parser.add_argument("--accelerator", default='gpu')
+    parser.add_argument("--devices", default=None)
+    args = parser.parse_args()
+    run(base_directory, args.accelerator, args.devices)

src/transformer_model.py
@@ -6,7 +6,7 @@ class TransformerModel(nn.Module):
     """
     PyTorch Lightning module for a generic Transformer model.
     """
-    def __init__(self, input_dim=31, model_dim=512, num_heads=32, num_layers=3, output_dim=6, dropout=0.1):
+    def __init__(self, input_dim=31, model_dim=256, num_heads=16, num_layers=2, output_dim=6, dropout=0.1):
         super().__init__()
         self.model_dim = model_dim
         self.embedding = nn.Linear(input_dim, model_dim)

src/utilities.py
@@ -3,7 +3,7 @@ import json
 import joblib
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import random_split, DataLoader
+from torch.utils.data import random_split
 from tqdm import tqdm
 import os
 import pandas as pd
@@ -73,8 +73,9 @@ def load_scaler(base_directory):
 
     return joblib.load(file)
 
-def load_data(directory, cache_directory, memory, train_perc, val_perc, device, forecast_days = [1, 7, 15]):
+def load_data(base_directory, memory, train_perc, val_perc, device, forecast_days = [1, 7, 15]):
     rv = []
+    cache_directory = get_cache_directory(base_directory)
     cache_json_file = os.path.join(cache_directory, "preprocessed.json")
     if os.path.exists(cache_json_file):
         with open(cache_json_file, "r") as f :
@@ -92,11 +93,12 @@ def load_data(directory, cache_directory, memory, train_perc, val_perc, device, 
     train_index_start = 0
     val_index_start = 0
     test_index_start = 0
-    scaler = load_scaler(directory, cache_directory)
-    for f in tqdm(os.listdir(directory)):
-        file = os.path.join(directory, f)
+    scaler = load_scaler(base_directory)
+    csv_directory = get_csv_directory(base_directory)
+    for f in tqdm(os.listdir(csv_directory)):
+        file = os.path.join(csv_directory, f)
         original_df = pd.read_csv(file, sep=',', index_col=False)
-        df = scaler.transform(original_df)
+        df = pd.DataFrame(scaler.transform(original_df), index=original_df.index, columns=original_df.columns)
         input,output = convert_to_sequences(memory, forecast_days, df.to_numpy())
         train_size = int(len(input) * train_perc);
         val_size = int(len(input) * val_perc);